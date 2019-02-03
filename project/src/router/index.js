4cd247d49fe05b584cefdd7e7e11ace114deb807
07JP27 <jynpei.t@gmail.com>
2019-02-03 10:36:50 +0900

    history設定

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -36,6 +36,11 @@ export default new Router({
       path: '/profile',
       name: 'Profile',
       component: Profile
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
